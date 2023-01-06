68acd38df89394f283da2b440227100543937224
Lauren Taplin <ltaplin@poppulo.com>
2023-01-06 12:51:11 +0000

    remove terraform m1 workaround

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -1,7 +1,3 @@
-# Terraform (M1)
-# https://stackoverflow.com/questions/70007818/terraform-the-plugin-encountered-an-error-and-failed-to-respond-to-the-plugin
-export GODEBUG=asyncpreemptoff=1
-
 # AWS
 export AWS_PAGER=""
 export AWS_DEFAULT_REGION="eu-west-1"
