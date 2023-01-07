916af327dc3a5ce805c6c3b53a9cdbb19c422cf9
Lauren Taplin <laurentap96@gmail.com>
2023-01-07 20:07:04 +0000

    fix p10k env issue

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -6,3 +6,8 @@ export AWS_SDK_LOAD_CONFIG=1
 # PGP
 tty=$(tty)
 export GPG_TTY=$tty
+
+# https://github.com/romkatv/powerlevel10k/issues/642#issuecomment-622962339
+if [[ "$TERM" != "xterm-256color" ]]; then
+    export TERM=xterm-256color
+fi
